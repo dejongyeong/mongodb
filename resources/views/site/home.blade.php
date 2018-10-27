0de59a04863ce12258640fa3a54052a69414fc33
De Jong <dejong.re2@gmail.com>
2018-10-27 20:18:41 +0100

    Heading and Insert Book Button

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -3,6 +3,13 @@
 {{-- Content --}}
 @section('content')
     
-<p>ssss</p>
+    <div class='row' style="border-bottom:1px solid grey;">
+        <div class='col-sm-9 pl-0'>
+            <h2>Books Collection</h2>
+        </div>
+        <div class='col-sm-3 pull-right p-0'>
+            <button type='button' class='float-right btn btn-secondary btn-sm mt-1 btn-size'><span class="fas fa-sm fa-plus-circle"></span> &nbsp; Insert Book</button>
+        </div>
+    </div>
 
 @endsection
