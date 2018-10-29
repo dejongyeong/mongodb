75a60c734c14c8523c260a6ce5e55e8e66430218
De Jong <dejong.re2@gmail.com>
2018-10-29 19:53:11 +0000

    User Interface implementation for Create Operation

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -34,12 +34,6 @@
         window.Laravel = <?php echo json_encode([
             'csrfToken' => csrf_token(),
         ]); ?>
-
-        window.setTimeout(function() {
-            $('.alert').fadeTo(500,0).slideUp(500, function() {
-                $(this).remove();
-            });
-        }, 3000);
     </script>
 
 </head>
--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -18,7 +18,9 @@
             <h2>Books Collection</h2>
         </div>
         <div class='col-sm-3 pull-right p-0'>
-            <button type='button' class='float-right btn btn-secondary btn-sm mt-1 btn-size'><span class="fas fa-sm fa-plus-circle"></span> &nbsp; Insert Book</button>
+            <button type='button' class='float-right btn btn-secondary btn-sm mt-1 btn-size' data-toggle='modal' data-target='#create-modal'>
+                <span class="fas fa-sm fa-plus-circle"></span> &nbsp; Insert Book
+            </button>
         </div>
     </div>
 
@@ -69,4 +71,20 @@
         @endif
     </div>
 
+@endsection
+
+<!-- Create -->
+@section('modals')
+    @include('crud.create')
+@endsection
+
+<!-- JavaScript -->
+@section('script')
+    <script type="text/javascript">
+        window.setTimeout(function() {
+            $('.alert').fadeTo(500,0).slideUp(500, function() {
+                $(this).remove();
+            });
+        }, 3000);
+    </script>
 @endsection
